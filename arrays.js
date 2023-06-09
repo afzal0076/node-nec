d6e5b9740a562a56d0a4c03aa2629cfcbf2ff776
Afzal <afzalah747@gmail.com>
2023-06-09 14:31:40 +0530

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,25 @@
+// const arr1 = ["entu", "bintu"];
+// const arr2 = ["chintu", "pintu", "kittu"];
+// const children = arr1.concat(arr2);
+// console.log(children);
+
+// const arr = [1,2,3,4,5];
+// const mapped = arr.map(element => element * 2);
+// console.log(mapped);
+
+// const arr = [1,2,3,4,5];
+// const filtered = arr.filter(element => element === 1 || element === 3);
+// console.log(filtered);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.sort();
+// console.log(fruits);
+// fruits.reverse();
+// console.log(fruits);
+
+// const numbers = [65, 44, 12, 4];
+// numbers.forEach(element => {console.log(element)});
+
+const arr = [1,2,3,4,5];
+const greaterfive=arr.every(num=>num>5);
+console.log(greaterfive);
